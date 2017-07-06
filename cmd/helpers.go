45b214accc97ebef44ba76f1b9d34c8a8605012f
callmeradical <lars@paniksrvr.info>
2017-07-05 23:45:50 -0400

    initial commit

---
+++ cmd/helpers.go
@@ -0,0 +1,90 @@
+package cmd
+
+import (
+	"errors"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"gopkg.in/yaml.v2"
+)
+
+func WorkLogPath(p string, f string) string {
+	return filepath.Join(os.Getenv("HOME"), p, f)
+}
+func ArchiveLog(p string, f string) error {
+	archive := filepath.Join(os.Getenv("HOME"), p, "worklog_"+time.Now().Format(time.RFC3339)+".csv")
+	filename := WorkLogPath(p, f)
+	return os.Rename(filename, archive)
+}
+
+func CreateLog(f string) error {
+
+	file, err := os.Create(f)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	_, err = file.Write([]byte("date, action\n"))
+
+	return err
+}
+
+func LogNotExists(f string) bool {
+	_, err := os.Stat(f)
+	return os.IsNotExist(err)
+}
+func LogExists(f string) bool {
+	return !LogNotExists(f)
+}
+func UpdateConfig(key string, value string) error {
+	filename := filepath.Join(os.Getenv("HOME"), ".worklog.yaml")
+	f, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Println(err.Error())
+		return err
+	}
+
+	c := map[string]string{}
+	err = yaml.Unmarshal(f, c)
+	if err != nil {
+		log.Println(err.Error())
+		return err
+	}
+
+	c[key] = value
+
+	b, _ := yaml.Marshal(c)
+
+	return ioutil.WriteFile(filename, b, 0666)
+
+	return nil
+}
+
+func WriteToWorkLog(path string, s string) error {
+	if LogNotExists(path) {
+		return errors.New("CurrentLog does not exist!")
+	}
+
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, 0666)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if _, err := f.WriteString(TimeStampActivity(s)); err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func TimeStampActivity(s string) string {
+	t := time.Now().Format(time.RFC3339)
+	activity := strings.Join([]string{t, s}, ",")
+	return activity + "\n"
+}
---
+++ cmd/new.go
@@ -0,0 +1,80 @@
+// Copyright © 2017 Lars Cromley lars@callmeradical.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// newCmd represents the new command
+var newCmd = &cobra.Command{
+	Use:   "new",
+	Short: "create a new weekly worklog",
+	Run: func(cmd *cobra.Command, args []string) {
+		path := viper.GetString("LogDir")
+		if path == "" {
+			fmt.Println("LogDir is not set")
+		}
+
+		filename := viper.GetString("CurrentLog")
+		if filename == "" {
+			fmt.Println("No file found, generating new file...")
+			err := UpdateConfig("CurrentLog", "current_log.csv")
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		if LogExists(WorkLogPath(path, filename)) {
+			archiveLogPath := viper.GetString("ArchiveLogDir")
+			if archiveLogPath == "" {
+				archiveLogPath = path
+				err := UpdateConfig("ArchiveLogDir", path)
+				if err != nil {
+					panic(err)
+				}
+			}
+
+			err := ArchiveLog(path, filename)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		err := CreateLog(WorkLogPath(path, filename))
+		if err != nil {
+			panic(err)
+		}
+
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(newCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// newCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// newCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+}
